18b7f6183ac3857df9b3e03c62af5db927887f82
weiyinfu.weiyinfu <weiyinfu.weiyinfu@bytedance.com>
2022-03-05 13:50:41 +0800

    good

--- learnCsharp/07-枚举.cs
+++ learnCsharp/07-枚举.cs
@@ -19,5 +19,9 @@ class UseEnum
         int y = (int) Days.Fri;
         Console.WriteLine("Sun = {0}", x);
         Console.WriteLine("Fri = {0}", y);
+
+        //枚举强制类型转换
+        Days dd = (Days) 100;
+        Console.Write($"{dd}");
     }
 }

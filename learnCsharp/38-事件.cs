2772a0c0dd8d239549bf8e577638a62b8020ae89
weiyinfu.weiyinfu <weiyinfu.weiyinfu@bytedance.com>
2023-02-26 03:16:07 +0800

    rm bin

--- learnCsharp/38-事件.cs
+++ learnCsharp/38-事件.cs
@@ -33,3 +33,74 @@ class EventIsDelegate
         b.click(); //output two times
     }
 }
+
+public class EventTest2
+{
+    public class Publisher
+    {
+        // Declare the delegate (if using non-generic pattern).
+        public delegate void SampleEventHandler(object sender, string e);
+
+        // Declare the event.
+        public event SampleEventHandler SampleEvent;
+
+        // Wrap the event in a protected virtual method
+        // to enable derived classes to raise the event.
+        public void RaiseSampleEvent()
+        {
+            // Raise the event in a thread-safe manner using the ?. operator.
+            SampleEvent?.Invoke(this, new string("Hello"));
+        }
+    }
+
+    public class Subscriber
+    {
+        private static int id;
+        public int ID;
+
+        public Subscriber()
+        {
+            this.ID = id++;
+        }
+
+        public void handle(object sender, string s)
+        {
+            Console.WriteLine($"{ID} handle {s}");
+        }
+    }
+
+    public static void TestEvent()
+    {
+        Publisher pub = new Publisher();
+        Subscriber x = new Subscriber();
+        Subscriber y = new Subscriber();
+        pub.SampleEvent += x.handle;
+        pub.SampleEvent += y.handle;
+        pub.RaiseSampleEvent();
+    }
+}
+
+public class TestAction
+{
+    public static void ActionAdd()
+    {
+        Action one = () => { Console.WriteLine("one"); };
+        Action two = () => { Console.WriteLine("two"); };
+        one += two;
+        one.Invoke();
+        Console.WriteLine("===========");
+        Action three = null;
+        three += one;
+        three += two;
+        three.Invoke();
+    }
+
+    public static void ActionAdd2()
+    {
+        Action one = () => { Console.WriteLine("one"); };
+        Action two = () => { Console.WriteLine("two"); };
+        var four = one + two;
+        four.Invoke();
+        four(); //直接使用括号就能够执行
+    }
+}
